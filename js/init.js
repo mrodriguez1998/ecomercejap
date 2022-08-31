d5bba6d9d268de8dbea552bbf1b572a955978763
mrodriguez1998 <mauror2012@gmail.com>
2022-08-30 21:26:39 -0300

    2/3 segunda entrega
    guardado de email y aparicion en barra sup lado derecho OK
    
    copiado funcionalidad de categories.html en products(sin funcionar, solo html)

--- js/init.js
+++ js/init.js
@@ -40,3 +40,9 @@ let getJSONData = function (url) {
       return result;
     });
 }
+//esto ultimo, al cargar el script, carga en el elemento de id "user" lo que ingresamos en el campo de "email" en el login
+document.addEventListener("DOMContentLoaded", function(){
+
+  document.getElementById("user").innerHTML = localStorage.getItem("usuario")
+}
+)
--- js/login.js
+++ js/login.js
@@ -1,3 +1,7 @@
+
+
+
+
 document.getElementById("boton").addEventListener("click",function(){
     let email = document.getElementById("email").value;
     let password = document.getElementById("contraseña").value;
@@ -14,6 +18,8 @@ document.getElementById("boton").addEventListener("click",function(){
     }
 
     if(condiciones){
+        localStorage.setItem("usuario", email);
+        //con esto dejamos seteado el email para poder mostrarlo luego en la punta derecha sup de la pantalla
         window.location.href = "login.html";
     }
 })
