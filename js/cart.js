74a80a944ca04de7405d3968ada169a32dd4c070
mrodriguez1998 <mauror2012@gmail.com>
2022-10-14 21:04:34 -0300

    entrega 5 segundo encare
    funciona el onchange para la actualizacion de precio y el input de cantidad

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,8 @@
+var carrito = {}
+var cantidad = 1
 
 document.addEventListener("DOMContentLoaded", function(){
+    //al cargar la pagina, mete en el html la info en formato tabla de el articulo
     getJSONData("https://japceibal.github.io/emercado-api/user_cart/25801.json").then(function (resultObj) {
         if (resultObj.status === "ok") {
             carrito = resultObj.data.articles[0]
@@ -19,11 +22,19 @@ document.addEventListener("DOMContentLoaded", function(){
             <td><img src="${carrito.image}" width=50px class=""></td>
             <td>${carrito.name}</td>
             <td>U$S${carrito.unitCost}</td>
-            <td>1</td>
-            <td><b>${carrito}</b></td>
+            <td><input type="number" id="cantidadArticulo" style="width: 50px;" min="1" value="1" onchange="costoFinal()" required></td>
+            <td><b><div id="costoTotal"></div></b></td>
             </tr>
             </table>
             `
+            costoFinal()//la ejecutamos aca para que cuando cargue la pagina, comience ya teniendo costo final
         }})})
+
+    function costoFinal(){
+        cantidad = document.getElementById("cantidadArticulo").value
+        document.getElementById("costoTotal").innerHTML =("U$S")+(carrito.unitCost)*(cantidad) 
+        //toma el valor del articulo, y lo multiplica por la cantidad en el momento para luego meterlo en el 
+        //html,   
+    }
 
        
